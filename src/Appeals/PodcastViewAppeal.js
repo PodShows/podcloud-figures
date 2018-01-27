71fbaf1d63e39f6810a1b9d7bc1b7f6e50e2b87f
Pof Magicfingers <pof@pof.pm>
2018-01-27 22:45:23 +0100

    Fix feed id

--- src/Appeals/PodcastViewAppeal.js
+++ src/Appeals/PodcastViewAppeal.js
@@ -94,7 +94,7 @@ PodcastViewAppeal.process = payload => {
 
     const verified_payload = verifyPayload(payload)
 
-    const feed_id = View.ObjectId(verified_payload.feed_id)
+    const feed_id = View.ObjectId(verified_payload.fid)
     const ip = validIPOrDefault(verified_payload.ip)
     const user_agent = validUserAgentOrDefault(verified_payload.ua)
     const referer = validRefererOrDefault(verified_payload.ref)
