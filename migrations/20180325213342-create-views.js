02fc5056cb59b5d8669472a00c9fd4f780f52a65
Pof Magicfingers <pof@pof.pm>
2018-04-09 23:00:07 +0200

    Update and add migration

--- migrations/20180325213342-create-views.js
+++ migrations/20180325213342-create-views.js
@@ -5,40 +5,40 @@ var type;
 var seed;
 
 /**
-  * We receive the dbmigrate dependency from dbmigrate initially.
-  * This enables us to not have to rely on NODE_PATH.
-  */
+ * We receive the dbmigrate dependency from dbmigrate initially.
+ * This enables us to not have to rely on NODE_PATH.
+ */
 exports.setup = function(options, seedLink) {
-  dbm = options.dbmigrate;
-  type = dbm.dataType;
-  seed = seedLink;
+    dbm = options.dbmigrate;
+    type = dbm.dataType;
+    seed = seedLink;
 };
 
 exports.up = function(db) {
-  return db.createTable('views' {
-    'source': 'varchar',
-    'feed_id': 'uuid',
-    'ip': 'inet',
-    'user_agent': 'text',
-    'city': 'map',
-    'country': 'varchar',
-    'referer': 'varchar',
-    'referer_host': 'varchar',
-    'daily_timecode': 'int',
-    'daily_timecode_with_ip': 'varchar',
-    'monthly_timecode': 'int',
-    'monthly_timecode_with_ip': 'varchar',
-    'created_at': 'timestamp',
-    'updated_at': 'timestamp'
-  }, {
-    'primary_key': 'feed_id, source'
-  });
+    return db.createTable('views' {
+        'source': 'varchar',
+        'feed_id': 'uuid',
+        'ip': 'inet',
+        'user_agent': 'text',
+        'city': 'map',
+        'country': 'varchar',
+        'referer': 'varchar',
+        'referer_host': 'varchar',
+        'daily_timecode': 'int',
+        'daily_timecode_with_ip': 'varchar',
+        'monthly_timecode': 'int',
+        'monthly_timecode_with_ip': 'varchar',
+        'created_at': 'timestamp',
+        'updated_at': 'timestamp'
+    }, {
+        'primary_key': '(feed_id, source)'
+    });
 };
 
 exports.down = function(db) {
-  return db.dropTable('views');
+    return db.dropTable('views');
 };
 
 exports._meta = {
-  "version": 1
+    "version": 1
 };
