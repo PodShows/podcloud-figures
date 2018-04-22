5b68cdf5224e7ceadf8e4bb2fe5011a4ee3f4cc1
Pof Magicfingers <pof@pof.pm>
2018-04-22 20:28:58 +0200

    Reset development to fresh boilerplate

--- deploy.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-npm install -g pm2
-pm2 deploy "${1}"
--- src/Utils/SignedPayload/Errors/InvalidIssuerPayloadError.js
+++
@@ -1,5 +0,0 @@
-import InvalidPayloadError from "./InvalidPayloadError"
-
-export class InvalidIssuerPayloadError extends InvalidPayloadError {}
-
-export default InvalidIssuerPayloadError
