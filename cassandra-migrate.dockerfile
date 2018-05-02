ce06eb55583f198265e58623f4d054cc9373d48c
Pof Magicfingers <pof@podcloud.fr>
2018-05-02 14:05:05 +0200

    Add migrations scripts

--- cassandra-migrate.dockerfile
+++
@@ -1,5 +0,0 @@
-FROM python
-
-RUN pip install --no-cache-dir cassandra-migrate
-
-CMD [ "cassandra-migrate" ] 
--- cassandra-migrate.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-docker-compose up -d && 
-docker-compose run --rm -v "$PWD":/usr/src/myapp -w /usr/src/myapp cassandra_migrate cassandra-migrate $@
