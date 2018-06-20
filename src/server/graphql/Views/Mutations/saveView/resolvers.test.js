496bbab2a1802b60e6d0f1c682b507e94ed334d7
Pof Magicfingers <pof@podcloud.fr>
2018-06-20 14:30:36 +0200

    Add real insertion test

--- src/server/graphql/Views/Mutations/saveView/resolvers.test.js
+++ src/server/graphql/Views/Mutations/saveView/resolvers.test.js
@@ -12,24 +12,33 @@ describe("Views", () => {
       }
     });
 
-    test("resolve to true with correct arguments", async () => {
-      const db = new postgres.Client({
-        user: "postgres",
-        host: "localhost",
-        database: "podcloud_stats_test"
-      });
-
-      await db.connect();
-      const response = await saveView.saveView(
-        {
-          FeedID: "110e8400-e29b-11d4-a716-446655440000",
-          IP: "127.0.0.1",
-          UserAgent: "UserAgent",
-          Referer: "http://referer.com/"
-        },
-        await testContext()
-      );
+    test("save a view", async () => {
+      const context = await testContext();
+      const viewData = {
+        FeedID: "110e8400-e29b-11d4-a716-446655440000",
+        IP: "127.0.0.1",
+        UserAgent: "UserAgent test " + +new Date(),
+        Referer: "http://referer.com/"
+      };
+      const response = await saveView.saveView(viewData, context);
       expect(response).toBe(true);
+
+      const result = await new Promise((resolve, reject) => {
+        context.db.query(
+          `
+        SELECT * FROM views WHERE user_agent = $1
+      `,
+          [viewData.UserAgent],
+          (err, results) => {
+            if (err) {
+              return reject(err);
+            }
+            resolve(results);
+          }
+        );
+      });
+      expect(result).toHaveProperty('rowCount', 1);
+      expect(result.rows[0]).toHaveProperty('user_agent', viewData.UserAgent);
     });
   });
 });
